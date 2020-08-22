9766ccbd921ca6d358792716f0914cfd92c06337
Mohsen Bostan <mohsenbostandev@gmail.com>
2020-08-22 15:52:30 +0430

    [v1.1.2] Fix Tests :bug:

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -2,9 +2,7 @@
 
 namespace Mohsenbostan\LaravelSecretImage\Tests;
 
-use Illuminate\Http\File;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Mohsenbostan\LaravelSecretImage\LaravelSecretImage;
 use Orchestra\Testbench\TestCase;
 use Mohsenbostan\LaravelSecretImage\LaravelSecretImageServiceProvider;
@@ -15,6 +13,16 @@ class ImageTest extends TestCase
     protected function getPackageProviders($app): array
     {
         return [LaravelSecretImageServiceProvider::class];
+    }
+
+    /** @test */
+    public function it_can_get_image_url()
+    {
+        $fake_image_name = Str::random();
+        $expected_url = route('secret-image.show-image') . "?image=$fake_image_name";
+        $actual_url = LaravelSecretImage::getImageUrl($fake_image_name);
+
+        self::assertSame($expected_url, $actual_url);
     }
 
 }
